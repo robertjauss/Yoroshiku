13998ecf23672ae63fdbf6adfc9ff904296b1f29
Robert Jauss <robertjauss@gmail.com>
2018-09-28 01:47:53 -0700

    Commenting, removing unneeded computeds

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -26,47 +26,43 @@ export default {
     },
     methods: {
         select(activity) {
+            // Takes in an activity and checks if it's 'coffee' or not, returns 1 or 3-5 random choices from the list of coworkers
             if (activity == 'coffee') {
                 let coworker = this.getCoworkers(1)
-                if (coworker.length > 0) {
+                if (coworker[0]) { // Checks if null list was passed back
                     this.group = coworker
                     this.localStorage.hadCoffee.push(coworker[0])
                 } else {
                     this.group = null
                 }
             } else {
-                let rand = Math.floor(Math.random() * 3) + 3
-                let lunchGroup = this.getCoworkers(rand)
-                this.group = lunchGroup
-                this.localStorage.hadLunch.concat(lunchGroup)
+                let rand = Math.floor(Math.random() * 3) + 3  // Random number between 3 and 5
+                let lunchGroup = this.getCoworkers(rand)      
+                this.group = lunchGroup                       
+                this.localStorage.hadLunch.concat(lunchGroup) // Combines list of members to localStorage list
             }
             this.modal = true
         },
         getCoworkers(n) {
+            // Does random selection based on passed number, n, provided by above function based on activity
             let available = []
             if (n == 1) {
+                // Checks localStorage for list of coworkers and filters those out of the 'available' list for coffee
                 available = this.coworkers.filter(c => !this.localStorage.hadCoffee.includes(c))
             } else {
+                // parsing a stringified list ensures we are not mutating the original list and only a copy
                 available = JSON.parse(JSON.stringify(this.coworkers))
             }
             let going = []
             for (let i = 0; i < n; i++) {
-                let person = available[Math.floor(Math.random() * available.length)]
-                going.push(person)
-                available.splice(available.indexOf(person), 1)
+                let person = available[Math.floor(Math.random() * available.length)] // Uses Math functions to pick random number of elements from 'available' list
+                going.push(person)                                                   
+                available.splice(available.indexOf(person), 1) // Removes added person from 'available' list to avoid duplication
             }
             return going
         },
         closeModal() {
-            this.modal = false
-        }
-    },
-    computed: {
-        hadCoffee() {
-            return this.localStorage.hadCoffee
-        },
-        hadLunch() {
-            return this.localStorage.hadLunch
+            this.modal = false // Creating function externally and passing in as we cannot mutate props internally
         }
     }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,6 +20,7 @@ export default {
   },
   computed: {
     name() {
+      // Pulls 'name' object from localStorage, which avoids opening the set modal
       return this.localStorage.name
     }
   }
@@ -28,23 +29,11 @@ export default {
 </script>
 
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #app {
   background: url('../assets/bg.png')
 }
 h1, h2 {
   font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
